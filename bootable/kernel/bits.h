41ed77e0461659cadddd57d49220ff7a9dbd29a4
Dimitris Vidos <dvidos@gmail.com>
2022-05-13 16:14:11 -0400

    Bootable moved to separate repo handcrafted-os

--- bootable/kernel/bits.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BITS_H
-#define _BITS_H
-
-#define SET_BIT(value, bitno)      ((value) | (1 << (bitno)))
-#define CLEAR_BIT(value, bitno)    ((value) & ~(1 << (bitno)))
-#define IS_BIT(value, bitno)       ((value) & (1 << (bitno)))
-
-
-#endif

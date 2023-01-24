27d5c2f5a6513d24761b4ca536c3815d1a6aeaaa
Dimitris Vidos <dvidos@gmail.com>
2023-01-24 17:05:36 -0500

    Restructuring folders and cleanup

---
+++ defs.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdbool.h"
+
+#define DONE     1  // implies success, but also EOF or similar
+#define SUCCESS  0
+#define ERROR   -1
+
+extern bool verbose;
---
+++ parser/shunting_yard.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../ast_node.h"
+
+expr_node *parse_expression_using_shunting_yard();
+
